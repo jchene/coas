50154111337851b08262026fdceeacb79c2730e9
jchene@student.42.fr <jchene@student.42.fr>
2022-11-08 22:29:35 +0100

    adding staff info

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
+function cleanContainer(){
+  $(".imgtext").remove();
+  $(".imgcol").remove();
+  $(".txtcol").remove();
+}
+
 function fillRows(){
 	for (let i = 2; i < 6; i++)
 		$("<div></div>").attr("class", "row row" + i).appendTo($(".container"))
 }
 
 function loadImages(){
-	$(".imgcol").remove();
+	cleanContainer()
 	var coas = ["order", "federation", "alliance", "assembly"];
 	for (let i = 0; i < 4; i++){
 		var img = $("<img>").attr("src", "ressources/" + coas[i] + "_background.jpg").attr("class", "img");
-		var imgboard = $("<div></div>").attr("class", "col s3 imgcol " + coas[i]);
+		var imgboard = $("<div></div>").attr("class", "col s3 imgcol " + coas[i] + "img");
 		img.appendTo(imgboard);
 		imgboard.appendTo($(".row2"));
 	}
@@ -16,32 +22,33 @@ function loadImages(){
 
 function displayLeaderboard(){
 	$(".imgtext").remove();
-	var coas = ["order", "federation", "alliance", "assembly"];
-	var boardRow = $(".row3")
+  $(".txtcol").remove();
+	var coas = ["order", "federation", "alliance", "assembly"];  
 	for (let i = 0; i < 4; i++){
 		var p = $("<p>The " + coas[i].charAt(0).toUpperCase() + coas[i].slice(1) + "</p>").attr("class", "imgtext");
-	  var	board = $("<div></div>").attr("class", "col s3 txtcol");
+	  var	board = $("<div></div>").attr("class", "col s3 txtcol " + coas[i] + "txt1");
 	  p.appendTo(board);
-	  board.appendTo(boardRow);
+	  board.appendTo($(".row3"));
+	  p = $("<p>100 000</p>").attr("class", "imgtext");
+	  board = $("<div></div>").attr("class", "col s3 txtcol " + coas[i] + "txt2");
+	  p.appendTo(board);
+	  board.appendTo($(".row4"));
 	}
-}
-
-function setStaff(name){
-	var img = $("<img>").attr("src", "ressources/" + name + "_background.jpg");
-	img.attr("class", "img");
-	var p = $("<p>The " + name.charAt(0).toUpperCase() + name.slice(1) + "</p>").attr("class", "imgtext");
-	var leaderboard = $("<div></div>").attr("class", "col s3 imgcol");
-	img.appendTo(leaderboard);
-	p.appendTo(leaderboard);
-	leaderboard.appendTo($(".row2"));
 }
 
 function displayStaff(){
 	$(".imgtext").remove();
+  $(".txtcol").remove();
 	var coas = ["order", "federation", "alliance", "assembly"];
 	for (let i = 0; i < 4; i++){
-		setStaff(coas[i]);
-	}
+		var img = $("<img>").attr("src", "ressources/medium_jchene.jpg");
+	  img.attr("class", "staffimg imgtext");
+  	var p = $("<p>Brume</p>").attr("class", "stafftxt imgtext");
+  	var staffBoard = $("<div></div>").attr("class", "col s3 txtcol");
+  	img.appendTo(staffBoard);
+    p.appendTo(staffBoard);
+  	staffBoard.appendTo($(".row4"));
+  }
 }
 
 $(document).ready(function() {
@@ -52,9 +59,10 @@ $(document).ready(function() {
 		displayLeaderboard();
 	})
 	$(".pbtn").click(function(){
-		loadImages();
+	  loadImages();
+		displayStaff();
 	})
 	$(".hbtn").click(function(){
-		$(".imgcol").remove();
+		cleanContainer();
 	})
 });
